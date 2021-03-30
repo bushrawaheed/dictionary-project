cb1d5991294a230d822d0fadd9810c302d2af19f
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-03-30 18:41:23 +0200

    remove unnecessary images

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
         Dictionary</header>
-        
-      <img src="https://images.unsplash.com/photo-1519882189396-71f93cb4714b?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8&w=300&q=80" className="App-img" alt="img"/>
     </div>
   );
 }
