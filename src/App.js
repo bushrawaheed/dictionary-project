ffa538f7b0096286b8a440d19c55c1ad581dee4c
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-03-31 11:37:00 +0200

    adding footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
         Dictionary</header>
+        <footer>coded by <a href="https://github.com/bushrawaheed/dictionary-project" target="_blank">Bushra Waheed</a></footer>
     </div>
   );
 }
