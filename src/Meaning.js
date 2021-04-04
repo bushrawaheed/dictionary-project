88965ba7004ed4c12c5a6c24b8a1afeea445c10c
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-04-04 23:11:29 +0200

    added css

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,6 +1,5 @@
 import React from "react";
 export default function Meaning(props){
-    console.log(props.meaning)
     return(
         <div className="Meaning">
            <h3>{props.meaning.partOfSpeech}</h3>
