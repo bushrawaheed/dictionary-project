39dfde7545c77e5ba1e2a21aa1cbe0f64e666cd5
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-04-05 10:44:15 +0200

    adding background color

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -3,8 +3,7 @@ export default function Synonyms(props){
  if(props.synonyms){
      return(
           <ul className="Synonyms">
-                        <strong>Synonyms:</strong>
-
+              <strong>Synonyms:</strong>
              {props.synonyms.map(function(synonym,index){
               return <li key={index}>{synonym}</li>;
              }
