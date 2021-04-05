1f97c31b4454da1c9e24315111184be8348b8b08
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-04-05 10:41:03 +0200

    rendering synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,7 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 export default function Meaning(props){
+    console.log(props.meaning);
     return(
         <div className="Meaning">
            <h3>{props.meaning.partOfSpeech}</h3>
@@ -7,10 +9,13 @@ export default function Meaning(props){
                return(
                    <div key={index}>
                        <p>
+                           <strong>Definition: </strong>
                      {definition.definition}
                      <br/>
+                     <strong>Example: </strong>
                      <em>{definition.example}</em>
-                       </p>               
+                       </p>         
+        <Synonyms synonyms={ definition.synonyms}/>      
                     </div>
                )
            })}
---
+++ src/Synonyms.js
@@ -0,0 +1,18 @@
+import React from "react";
+export default function Synonyms(props){
+ if(props.synonyms){
+     return(
+          <ul className="Synonyms">
+                        <strong>Synonyms:</strong>
+
+             {props.synonyms.map(function(synonym,index){
+              return <li key={index}>{synonym}</li>;
+             }
+             )}
+         </ul>
+     ) 
+     
+}else{
+    return "null";
+}
+}
